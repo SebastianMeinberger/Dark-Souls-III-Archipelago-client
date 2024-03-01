b848bf85b2b961f626f2358908e5c695658c1fe8
Sebastian Meinberger <sebastian.meinberger@rwth-aachen.de>
2024-03-01 13:25:14 +0100

    wrapped the subprojects to provide CMList txts if necessary

---
+++ archipelago-interface/CMakeFiles/archipelago-client.dir/cmake_clean.cmake
@@ -0,0 +1,34 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/archipelago-client.dir/ArchipelagoInterface.cpp.obj"
+  "CMakeFiles/archipelago-client.dir/ArchipelagoInterface.cpp.obj.d"
+  "CMakeFiles/archipelago-client.dir/AutoEquip.cpp.obj"
+  "CMakeFiles/archipelago-client.dir/AutoEquip.cpp.obj.d"
+  "CMakeFiles/archipelago-client.dir/Core.cpp.obj"
+  "CMakeFiles/archipelago-client.dir/Core.cpp.obj.d"
+  "CMakeFiles/archipelago-client.dir/GameHook.cpp.obj"
+  "CMakeFiles/archipelago-client.dir/GameHook.cpp.obj.d"
+  "CMakeFiles/archipelago-client.dir/ItemRandomiser.cpp.obj"
+  "CMakeFiles/archipelago-client.dir/ItemRandomiser.cpp.obj.d"
+  "CMakeFiles/archipelago-client.dir/NoRequirements.cpp.obj"
+  "CMakeFiles/archipelago-client.dir/NoRequirements.cpp.obj.d"
+  "CMakeFiles/archipelago-client.dir/dinput8.cpp.obj"
+  "CMakeFiles/archipelago-client.dir/dinput8.cpp.obj.d"
+  "CMakeFiles/archipelago-client.dir/subprojects/minhook/src/buffer.c.obj"
+  "CMakeFiles/archipelago-client.dir/subprojects/minhook/src/buffer.c.obj.d"
+  "CMakeFiles/archipelago-client.dir/subprojects/minhook/src/hde/hde32.c.obj"
+  "CMakeFiles/archipelago-client.dir/subprojects/minhook/src/hde/hde32.c.obj.d"
+  "CMakeFiles/archipelago-client.dir/subprojects/minhook/src/hde/hde64.c.obj"
+  "CMakeFiles/archipelago-client.dir/subprojects/minhook/src/hde/hde64.c.obj.d"
+  "CMakeFiles/archipelago-client.dir/subprojects/minhook/src/hook.c.obj"
+  "CMakeFiles/archipelago-client.dir/subprojects/minhook/src/hook.c.obj.d"
+  "CMakeFiles/archipelago-client.dir/subprojects/minhook/src/trampoline.c.obj"
+  "CMakeFiles/archipelago-client.dir/subprojects/minhook/src/trampoline.c.obj.d"
+  "libarchipelago-client.dll"
+  "libarchipelago-client.dll.a"
+  "libarchipelago-client.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C CXX)
+  include(CMakeFiles/archipelago-client.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ archipelago-interface/CMakeFiles/archipelago-client.dir/subprojects/minhook/src/hde/hde32.c.obj.d
@@ -0,0 +1,2 @@
+CMakeFiles/archipelago-client.dir/subprojects/minhook/src/hde/hde32.c.obj: \
+ /home/seb/Documents/Dark-Souls-III-Archipelago-client/archipelago-client/subprojects/minhook/src/hde/hde32.c
